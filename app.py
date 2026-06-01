4887a4745c79eee735147693e88691df306091e4
mohdtalal3 <muhammadtalal21052@gmail.com>
2026-06-01 19:31:16 +0300

    update port

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ if __name__ == "__main__":
     db.init_db()
     db.migrate_add_delay_seconds()
     #app.run(debug=True, port=5000)
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
